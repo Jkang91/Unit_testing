830a9d374d85796be745d34f1a380b5e763f8470
jkang91 <jkang34@gmail.com>
2022-06-22 12:07:27 -0400

    changed to chai and mocha

--- backend/controller/users/userController.test.js
+++ backend/controller/users/userController.test.js
@@ -1,11 +1,29 @@
 const mongoose = require('mongoose')
-const app = require("../../app")
-const request = require('supertest')
+const app = require('../../app')
+const {expect} = require('chai')
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+
+
+chai.use(chaiHttp);
+
+
 
 describe("Sign up process", () => {
-    it('tests /signup endpoint', async() => {
-        const response = await request(app).get("/auth/signup")
-        expect(response.statusCode).toBe(200)
+    it('successful sign up', (done) => {
+        chai.request(app)
+            .post("/auth/signup")
+            .send({
+                username: "jkang111",
+                password: "google",
+                email: "jkang11@gmail.com"
+            })
+            .end((error, res) => {
+                if(error) done(error);
+                expect(res).to.have.status(200)
+                expect(res).to.be.a("object")
+            })
+            done()
     })
 }) 
 
